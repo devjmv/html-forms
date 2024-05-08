483aae5e91f5258e050e2399b8fd125337d2e27a
devjmv <javier161890@gmail.com>
2024-05-08 14:04:27 +0200

    add js for show data

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function consolLog() {
+    console.log(document.getElementById('name').value);
+    console.log(document.getElementById('email').value);
+    console.log(document.getElementById('age').value);
+    console.log(document.getElementById('describe').value);
+    console.log(document.getElementById('input').value);
+}
